5a53825e547cb09212f7b02ca0663a7a3a3d6391
连瑞 <2984171269@qq.com>
2024-08-28 16:35:37 +0800

    预约

--- pages/registration.vue
+++ pages/registration.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="top-div">
-    <div class="span1">
-      <span>预约挂号</span>
-    </div>
-    <br />
+  <NuxtLayout name="show"> </NuxtLayout>
+  <div class="re-top-div">
+    <div class="span1"></div>
 
     <el-input style="width: 90%" placeholder="搜索门诊、医生">
       <template #prefix>
@@ -12,30 +10,18 @@
     </el-input>
   </div>
   <!-- 分类 -->
-
-  <el-menu style="width: 200px; height: calc(100vh - 50px)" :default-active="2">
-    <el-menu-item index="1-4-1" @Click="onChoose">眼科</el-menu-item>
-    <el-menu-item index="1-4-2">儿科</el-menu-item>
-    <el-menu-item index="1-4-3">内科</el-menu-item>
-    <el-menu-item index="1-4-4">外科</el-menu-item>
-  </el-menu>
-
-  <NuxtLayout name="tabbar"> </NuxtLayout>
 </template>
 <script setup>
 import { Search } from "@element-plus/icons-vue";
-const onChoose = () => {
-  navigateTo("/reg2", { replace: true });
-};
 </script>
 <style>
-.top-div {
+.re-top-div {
   text-align: center;
   font-size: 26px;
   background-color: #69c98b;
-  height: 150px;
+  height: 70px;
 }
-.top-div .span1 {
+.re-top-div .span1 {
   padding-top: 15px;
 }
 </style>
